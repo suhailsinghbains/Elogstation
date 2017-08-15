898a21c0bf488c520bf10cd3b765074597b6c515
Suhail Singh Bains <suhail.singh.bains@hotmail.com>
2017-08-15 18:51:04 +0530

    Initial

---
+++ Elogstation/Elogstation/HomePage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using ModernHttpClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elogstation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task SaveTodoItemAsync(string Username, string Password)
+        {
+            var json = JsonConvert.SerializeObject(new { username = Username, password = Password });
+            string data = json.ToString();
+            HttpClient client = new HttpClient(new NativeMessageHandler())
+            {
+                MaxResponseContentBufferSize = 256000
+            };
+            var RestUrl = "http://54.208.49.250/api/auth";
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+            var request = await client.PostAsync(RestUrl, content);
+            request.Headers.Add("postman-token", "5d07c886-04b0-6880-16ed-7b025bdef6ad");
+            var response = await request.Content.ReadAsStringAsync();
+            if (request.IsSuccessStatusCode)
+            {
+                Test.Text = response;
+                await Task.Delay(1000);
+                await Navigation.PushAsync(new Page1(Username));
+            }
+            else
+            {
+                Test.Text += "LOGIN Failed with the following error\n";
+                Test.Text = response;
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await SaveTodoItemAsync(Username.Text, Password.Text);
+        }
+    }
+}
---
+++ Elogstation/Elogstation/Page1.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Android.Net.Wifi;
+using CoreLocation;
+using ModernHttpClient;
+using Newtonsoft.Json;
+using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Connectivity;
+using Android.Content;
+using Android.Bluetooth;
+
+namespace Elogstation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        /*private readonly GeocoordinateSatelliteData _dataManager ;
+        private Location location;
+        public LocationDetail()
+        {
+            _dataManager = new GeocoordinateSatelliteData();
+        }*/
+        public Page1(string parameter)
+        {
+            InitializeComponent();
+            this.Title = "Hi" + " " + parameter;
+            Send_Data_Defi.IsVisible = false;
+            Map_View_Defi.IsVisible = false;
+            Logs_Defi.IsVisible = true;
+            D_Logs.BackgroundColor = Color.FromHex("#0d47a1");
+            D_Map_View.BackgroundColor = Color.FromHex("#3E88F2");
+            D_Send_Data.BackgroundColor = Color.FromHex("#3E88F2");
+            //If Wifi or Bluetooth is on beforehand, the respective button should be toggled
+            string T = "eyJleHAiOjE1MDI2MDAwNDIsImlhdCI6MTUwMTk5NTI0MiwiYWxnIjoiSFMyNTYifQ.eyJpZCI6Nn0.-s1xGqei94vva2A79CyMRA8mQA-ZCcfgLvlleRlHeLE";
+            //GPS_Api_CallAsync(T, 51.50336401, -0.1276250, 1, 1, 20);
+            GPS_Location();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            WifiManager wifi = (WifiManager)Android.App.Application.Context.GetSystemService(Context.WifiService);
+            wifi.SetWifiEnabled(e.Value);
+        }
+
+        private WifiManager getSystemService(object wIFI_SERVICE)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Switch_Toggled_1(object sender, ToggledEventArgs e)
+        {
+            BluetoothManager _manager = (BluetoothManager)Android.App.Application.Context.GetSystemService(Context.BluetoothService);
+            if (e.Value)
+            {
+                _manager.Adapter.Enable();
+            }
+            else
+            {
+                _manager.Adapter.Disable();
+            }
+        }
+
+        private void D_Logs_Clicked(object sender, EventArgs e)
+        {
+            Send_Data_Defi.IsVisible = false;
+            Map_View_Defi.IsVisible = false;
+            Logs_Defi.IsVisible = true;
+            D_Logs.BackgroundColor = Color.FromHex("#0d47a1");
+            D_Map_View.BackgroundColor = Color.FromHex("#3E88F2");
+            D_Send_Data.BackgroundColor = Color.FromHex("#3E88F2");
+        }
+
+        private void D_Send_Data_Clicked(object sender, EventArgs e)
+        {
+            Map_View_Defi.IsVisible = false;
+            Logs_Defi.IsVisible = false;
+            Send_Data_Defi.IsVisible = true;
+            D_Send_Data.BackgroundColor = Color.FromHex("#0d47a1");
+            D_Map_View.BackgroundColor = Color.FromHex("#3E88F2");
+            D_Logs.BackgroundColor = Color.FromHex("#3E88F2");
+        }
+
+        private void D_Map_View_Clicked(object sender, EventArgs e)
+        {
+            Send_Data_Defi.IsVisible = false;
+            Logs_Defi.IsVisible = false;
+            Map_View_Defi.IsVisible = true;
+            D_Map_View.BackgroundColor = Color.FromHex("#0d47a1");
+            D_Logs.BackgroundColor = Color.FromHex("#3E88F2");
+            D_Send_Data.BackgroundColor = Color.FromHex("#3E88F2");
+        }
+        private async Task GPS_Api_CallAsync(string Token, double Latitude, double Longitude, int User_id, int Company_id, int Rpm)
+        {
+            var json = JsonConvert.SerializeObject(new
+            {
+                token = Token,
+                user_id = User_id,
+                company_id = Company_id,
+                latitude = Latitude,
+                longitude = Longitude,
+                rpm = Rpm
+            });
+            string data = json.ToString();
+            HttpClient client = new HttpClient(new NativeMessageHandler())
+            {
+                MaxResponseContentBufferSize = 256000
+            };
+            var RestUrl = "http://54.208.49.250/api/eld";
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+            var request = await client.PostAsync(RestUrl, content);
+            var response = await request.Content.ReadAsStringAsync();
+            SentData.Text = response;
+        }
+
+        private async Task GPS_Location()
+        {
+            Test1.Text += "This is a test";
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(20), null, true);
+            Test1.Text += position;
+        }
+    }
+}
