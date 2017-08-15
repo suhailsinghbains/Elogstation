8474a6019c034a7af94e568e8ee59c464964beb7
Suhail Singh Bains <suhail.singh.bains@hotmail.com>
2017-08-15 21:03:09 +0530

    Adding Changes

--- Elogstation/Elogstation/Page1.xaml.cs
+++ Elogstation/Elogstation/Page1.xaml.cs
@@ -26,6 +26,8 @@ namespace Elogstation
         {
             _dataManager = new GeocoordinateSatelliteData();
         }*/
+        WifiManager wifi = (WifiManager)Android.App.Application.Context.GetSystemService(Context.WifiService);
+        BluetoothManager bluetooth = (BluetoothManager)Android.App.Application.Context.GetSystemService(Context.BluetoothService);
         public Page1(string parameter)
         {
             InitializeComponent();
@@ -36,33 +38,35 @@ namespace Elogstation
             D_Logs.BackgroundColor = Color.FromHex("#0d47a1");
             D_Map_View.BackgroundColor = Color.FromHex("#3E88F2");
             D_Send_Data.BackgroundColor = Color.FromHex("#3E88F2");
-            //If Wifi or Bluetooth is on beforehand, the respective button should be toggled
+            if(wifi.WifiState.ToString().Equals("Enabled"))
+            {
+                //Wifi Toggle ON
+            }
+            if (bluetooth.Adapter.IsEnabled)
+            {
+                //Bluetooth Toggle ON
+            }
             string T = "eyJleHAiOjE1MDI2MDAwNDIsImlhdCI6MTUwMTk5NTI0MiwiYWxnIjoiSFMyNTYifQ.eyJpZCI6Nn0.-s1xGqei94vva2A79CyMRA8mQA-ZCcfgLvlleRlHeLE";
             //GPS_Api_CallAsync(T, 51.50336401, -0.1276250, 1, 1, 20);
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             GPS_Location();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         private void Switch_Toggled(object sender, ToggledEventArgs e)
         {
-            WifiManager wifi = (WifiManager)Android.App.Application.Context.GetSystemService(Context.WifiService);
             wifi.SetWifiEnabled(e.Value);
         }
-
-        private WifiManager getSystemService(object wIFI_SERVICE)
-        {
-            throw new NotImplementedException();
-        }
-
+        
         private void Switch_Toggled_1(object sender, ToggledEventArgs e)
         {
-            BluetoothManager _manager = (BluetoothManager)Android.App.Application.Context.GetSystemService(Context.BluetoothService);
             if (e.Value)
             {
-                _manager.Adapter.Enable();
+                bluetooth.Adapter.Enable();
             }
             else
             {
-                _manager.Adapter.Disable();
+                bluetooth.Adapter.Disable();
             }
         }
 
